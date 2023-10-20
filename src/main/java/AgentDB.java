8d87fdac53f79ad61ab37ffa0885c1786c9a74ae
KhalezAlex <klozevitz@yandex.ru>
2023-10-21 00:49:45 +0300

    <feat> [db] <db connection> -open/close connections

--- src/main/java/AgentDB.java
+++ src/main/java/AgentDB.java
@@ -1,9 +1,27 @@
 import java.sql.*;
 
 public class AgentDB {
-    private static final String DB_URL = "";
-    private static final String DB_DRIVER = "";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/etalon";
+    private static final String DB_DRIVER = "org.postgresql.Driver";
+    private static final String username = "postgres";
+    private static final String password = "ItsMyLife0203";
     private Connection connection;
+
+    public AgentDB() {
+        try {
+            System.out.println("12");
+            Class.forName(DB_DRIVER);
+            System.out.println("14");
+            this.connection = DriverManager.getConnection(DB_URL, username, password);
+            System.out.println("connection established");
+        } catch (SQLException e) {
+            System.out.println("connection failed");
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC-driver not found: connection failed");
+            throw new RuntimeException(e);
+        }
+    }
 
     public AgentDB(String url) {
         try {
@@ -12,7 +30,7 @@ public class AgentDB {
             System.out.println("connection established");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            System.out.println("JDBC-driver not found!\nconnection failed");
+            System.out.println("JDBC-driver not found: connection failed");
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("connection failed");
@@ -27,5 +45,10 @@ public class AgentDB {
             System.out.println("connection failed");
             throw new RuntimeException(e);
         }
+    }
+
+    public void closeConnection() throws SQLException {
+        this.connection.close();
+        System.out.println("connection closed");
     }
 }
